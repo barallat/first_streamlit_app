1f311a0b12831ca61e1817d4c4259bdf9f584753
barallat <barallat@users.noreply.github.com>
2023-04-26 14:36:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import streamlit
 streamlit.title('My parents new healthy dinner')
 
 streamlit.header('Monday Breakfast')
-streamlit.text('Pan')
-streamlit.text('Aceite')
-streamlit.text('Sal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
